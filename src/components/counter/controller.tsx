601f8cb15653435888acbbfed24f0e4ffdfed46d
bestplus <minals0520@naver.com>
2025-11-01 22:06:37 +0900

    refactor: zustand 커스텀 훅 사용

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { useCountStore } from "@/store/count";
+import { useDecreaseCount, useIncreaseCount } from "@/store/count";
 
 export default function Controller() {
   // 선택자(Select) 함수를 사용해 특성 요소를 가져올 수 있다.
   // count의 값이 변경되더라도 전체 리랜더링을 막을 수 있음
-  const increase = useCountStore((store) => store.increase);
-  const decrease = useCountStore((store) => store.decrease);
+  const increase = useIncreaseCount();
+  const decrease = useDecreaseCount();
 
   return (
     <div>
