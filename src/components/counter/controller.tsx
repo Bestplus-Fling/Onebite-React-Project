b3a1c32e8669749fe66353280efa5948523261f6
bestplus <minals0520@naver.com>
2025-11-01 21:27:17 +0900

    refactor: controller 리랜더링 제한 - 선택자를 사용해 button 리랜더링 방지

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -2,7 +2,10 @@ import { Button } from "@/components/ui/button";
 import { useCountStore } from "@/store/count";
 
 export default function Controller() {
-  const { increase, decrease } = useCountStore();
+  // 선택자(Select) 함수를 사용해 특성 요소를 가져올 수 있다.
+  // count의 값이 변경되더라도 전체 리랜더링을 막을 수 있음
+  const increase = useCountStore((store) => store.increase);
+  const decrease = useCountStore((store) => store.decrease);
 
   return (
     <div>
