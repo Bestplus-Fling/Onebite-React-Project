5a3439d7c48709dbe952e5fa3c73483601a81227
bestplus <minals0520@naver.com>
2025-11-02 20:48:44 +0900

    feat: Todo 생성, 삭제 기능 추가

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { useDeleteTodo } from "@/store/todos";
+import type { Todo } from "@/types";
 
-export default function TodoItem({
-  id,
-  content,
-}: {
-  id: number;
-  content: string;
-}) {
+export default function TodoItem({ id, content }: Todo) {
+  const deleteTodo = useDeleteTodo();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
   return (
     <div className="flex items-center justify-between border p-2">
       {content}
-      <Button variant={"destructive"}>삭제</Button>
+      <Button onClick={handleDeleteClick} variant={"destructive"}>
+        삭제
+      </Button>
     </div>
   );
 }
