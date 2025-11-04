6d8f9969ea6fb829aa56279fe30b556573fc8244
bestplus <minals0520@naver.com>
2025-11-04 21:44:09 +0900

    refactor: cache 삭제와 reFetch 없이 데이터 추가 - queryClient.setQueryData를 이용해 캐시를 무효화하지 않고, 데이터를 직접 추가하도록 변경

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -12,9 +13,10 @@ export function useCreateTodoMutation() {
     // 요청이 종료되었을 때
     onSettled: () => {},
     // 요청이 성공했을 때
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     // 요청이 실패했을 때
