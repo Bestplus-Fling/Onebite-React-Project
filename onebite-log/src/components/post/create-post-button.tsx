f3551650e23f33b8bbb2b2693719554bdc377302
bestplus <minals0520@naver.com>
2025-11-18 23:12:40 +0900

    feat: 포스트 수정 - 포스트 모달에 update까지 반영되도록 수정 - update mutation 작성

--- onebite-log/src/components/post/create-post-button.tsx
+++ onebite-log/src/components/post/create-post-button.tsx
@@ -1,12 +1,12 @@
-import { useOpenPostEditorModal } from "@/store/post-editer-modal";
+import { useOpenCreatePostModal } from "@/store/post-editer-modal";
 import { PlusCircleIcon } from "lucide-react";
 
 export default function CreatePostButton() {
-  const openPostEditorModal = useOpenPostEditorModal();
+  const openCreatePostModal = useOpenCreatePostModal();
 
   return (
     <div
-      onClick={openPostEditorModal}
+      onClick={openCreatePostModal}
       className="bg-muted text-muted-foreground cursor-pointer rounded-xl px-6 py-4"
     >
       <div className="flex items-center justify-between">
